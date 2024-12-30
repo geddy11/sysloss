2e4a56365bf62481578d025e6ff3de0a4950849e
Geir <geir.drange@gmail.com>
2024-12-30 10:34:31 +0100

    feat: add heat diagram function, make_hdiag(), to diagram package

--- src/sysloss/diagram.py
+++ src/sysloss/diagram.py
@@ -30,9 +30,13 @@ from sysloss.system import System
 import copy, io
 import pydot
 from PIL import Image
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+from prefixed import Float
 
 
-__all__ = ["get_conf", "make_diag"]
+__all__ = ["get_conf", "make_diag", "make_hdiag"]
 
 
 _DEF_GRAPH_CONF = {
@@ -41,7 +45,9 @@ _DEF_GRAPH_CONF = {
     "splines": "line",
     "nodesep": "0.3",
     "overlap": "scale",
-    "dpi": "100",
+    "dpi": "120",
+    "fontname": "arial",
+    "fontcolor": "black",
 }
 _DEF_CLUSTER_CONF = {
     "default": {
@@ -49,6 +55,8 @@ _DEF_CLUSTER_CONF = {
         "fillcolor": "white",
         "style": "filled",
         "penwidth": "1.5",
+        "fontname": "arial",
+        "fontcolor": "black",
     }
 }
 _DEF_NODE_CONF = {
@@ -57,6 +65,8 @@ _DEF_NODE_CONF = {
         "style": "filled",
         "shape": "box",
         "penwidth": "1.5",
+        "fontname": "arial",
+        "fontcolor": "black",
     }
 }
 _DEF_EDGE_CONF = {
@@ -70,6 +80,17 @@ _DEF_CONF = {
     "cluster": _DEF_CLUSTER_CONF,
     "node": _DEF_NODE_CONF,
     "edge": _DEF_EDGE_CONF,
+}
+_COLD_RGB = "#2120ff"
+_WARM_RGB = "#ff1210"
+_DEF_GRADIENT = {
+    "fillcolor": _COLD_RGB + ":" + _WARM_RGB,
+    "style": "filled",
+    "gradientangle": "90",
+    "shape": "record",
+    "penwidth": ".0",
+    "fontcolor": "silver",
+    "fontname": "arial",
 }
 
 
@@ -92,6 +113,140 @@ def get_conf() -> dict:
     >>> make_diag(my_sys, config=my_conf)
     """
     return copy.deepcopy(_DEF_CONF)
+
+
+def _gcolor(mix: float):
+    """Calculate heat color from mix value (0-1)"""
+    c1 = np.array(mpl.colors.to_rgb(_COLD_RGB))
+    c2 = np.array(mpl.colors.to_rgb(_WARM_RGB))
+    return mpl.colors.to_hex((1 - mix) * c1 + mix * c2)
+
+
+def _prep_loss(loss: pd.DataFrame, phases: dict = {}) -> pd.DataFrame:
+    """Calculate loss and color for each component"""
+    if phases != {}:
+        df2 = loss[loss.Type != ""][["Component", "Loss (W)", "Phase"]]
+        avg, w = np.zeros(len(df2) // len(phases), dtype=np.float64), 0.0
+        for key in phases.keys():
+            avg += phases[key] * df2[df2.Phase == key]["Loss (W)"].to_numpy().astype(
+                np.float64
+            )
+            w += phases[key]
+        avg = avg / w
+        df = df2[df2.Phase == list(phases.keys())[0]].copy()
+        df.update(pd.DataFrame(({"Loss (W)": avg})))
+    else:
+        df = loss[loss.Type != ""][["Component", "Loss (W)"]].copy()
+
+    maxloss = df["Loss (W)"].max()
+    df["Mix"] = df["Loss (W)"].to_numpy() / maxloss
+    return df
+
+
+def _nice_float(f):
+    """Format floats with SI prefixes"""
+    pwr = int("{:e}".format(f).split("e")[1])
+    if pwr < -13 or pwr > 7:
+        return "{:.2e}".format(f)
+    elif pwr < -10:
+        return "{}p".format(round(f * 1e12, 3 - (13 + pwr)))
+    elif pwr < -7:
+        return "{}n".format(round(f * 1e9, 3 - (10 + pwr)))
+    elif pwr < -4:
+        return "{}u".format(round(f * 1e6, 3 - (7 + pwr)))
+    elif pwr < -1:
+        return "{}m".format(round(f * 1e3, 3 - (4 + pwr)))
+    elif pwr < 2:
+        return "{}".format(round(f, 3 - (1 + pwr)))
+    elif pwr < 5:
+        return "{}k".format(round(f / 1e3, 5 - pwr))
+    elif pwr < 8:
+        return "{}M".format(round(f / 1e6, 8 - pwr))
+
+
+def _diag(
+    sys: System,
+    *,
+    fname: str = None,
+    group: bool = True,
+    config: dict = {},
+    loss: pd.DataFrame = None
+):
+    """Create diagram"""
+    if config == {}:
+        bd_conf = copy.deepcopy(_DEF_CONF)
+    else:
+        bd_conf = copy.deepcopy(config)
+    gname = sys._g.attrs["name"]
+    if loss is not None:
+        gname += " - Loss heat map"
+    graph = pydot.Dot("sysLoss", label=gname, **bd_conf["graph"])
+
+    def add_node(gr, name, attrs, ldf):
+        comp = type(sys._g[sys._g.attrs["nodes"][name]]).__name__
+        conf = copy.deepcopy(attrs["default"])
+        # component type overrieds
+        if comp in attrs:
+            for key in attrs[comp]:
+                conf[key] = attrs[comp][key]
+        # component instance overrides
+        if name in attrs:
+            for key in attrs[name]:
+                conf[key] = attrs[name][key]
+        if ldf is not None:
+            conf["fillcolor"] = _gcolor(ldf[ldf.Component == name]["Mix"].to_list()[0])
+            conf["fontcolor"] = "silver"
+            conf["label"] = "{}\n{}W".format(
+                name, _nice_float(ldf[ldf.Component == name]["Loss (W)"].to_list()[0])
+            )
+        gr.add_node(pydot.Node(name, **conf))
+
+    # heat diagram operations
+    ldf = None
+    if loss is not None:
+        ldf = _prep_loss(loss, sys.get_sys_phases())
+
+    # find groups
+    groups = {}
+    for n in sys._g.attrs["groups"].keys():
+        g = sys._g.attrs["groups"][n]
+        if g != "":
+            groups[g] = 1
+    # create clusters
+    if group and groups != {}:
+        for g in groups.keys():
+            cconf = copy.deepcopy(bd_conf["cluster"]["default"])
+            if g in bd_conf["cluster"]:
+                for key in bd_conf["cluster"][g]:
+                    cconf[key] = bd_conf["cluster"][g][key]
+            sg = pydot.Subgraph("cluster_" + g, label=g, **cconf)
+            for n in sys._g.attrs["nodes"]:
+                if sys._g.attrs["groups"][n] == g:
+                    add_node(sg, n, bd_conf["node"], ldf)
+            graph.add_subgraph(sg)
+    # non-clustered nodes
+    for n in sys._g.attrs["nodes"]:
+        if sys._g.attrs["groups"][n] == "" or not group:
+            add_node(graph, n, bd_conf["node"], ldf)
+    # color gradient
+    if loss is not None:
+        gconf = copy.deepcopy(_DEF_GRADIENT)
+        gconf["label"] = "{}W|  |  | 0W".format(_nice_float(ldf["Loss (W)"].max()))
+        rd = bd_conf["graph"]["rankdir"]
+        if rd == "TB" or rd == "BT":
+            gconf["label"] = "{" + gconf["label"] + "}"
+        graph.add_node(pydot.Node("Scale", **gconf))
+    # edges
+    p = dict(zip(sys._g.attrs["nodes"].values(), sys._g.attrs["nodes"].keys()))
+    for e in iter(sys._g.edge_indices()):
+        ep = sys._g.get_edge_endpoints_by_index(e)
+        graph.add_edge(pydot.Edge(p[ep[0]], p[ep[1]], **bd_conf["edge"]))
+    # output image
+    if fname == None:
+        img = Image.open(io.BytesIO(graph.create_png(prog="dot")))
+        return img
+    graph.write(fname, prog="dot", format=fname.split(".")[-1])
+    return None
 
 
 def make_diag(sys: System, *, fname: str = None, group: bool = True, config: dict = {}):
@@ -130,56 +285,40 @@ def make_diag(sys: System, *, fname: str = None, group: bool = True, config: dic
     >>> my_conf["node"]["PSwitch"] = {"fillcolor":"aquamarine"}
     >>> make_diag(my_sys, fname="system.svg", config=my_conf)
     """
+    return _diag(sys, fname=fname, group=group, config=config, loss=None)
 
-    if config == {}:
-        bd_conf = copy.deepcopy(_DEF_CONF)
-    else:
-        bd_conf = copy.deepcopy(config)
-    graph = pydot.Dot("sysLoss", label=sys._g.attrs["name"], **bd_conf["graph"])
 
-    def add_node(gr, name, attrs):
-        comp = type(sys._g[sys._g.attrs["nodes"][name]]).__name__
-        conf = copy.deepcopy(attrs["default"])
-        # component type overrieds
-        if comp in attrs:
-            for key in attrs[comp]:
-                conf[key] = attrs[comp][key]
-        # component instance overrides
-        if name in attrs:
-            for key in attrs[name]:
-                conf[key] = attrs[name][key]
-        gr.add_node(pydot.Node(name, **conf))
+def make_hdiag(
+    sys: System, *, fname: str = None, group: bool = True, config: dict = {}
+):
+    """Create power tree heat diagram.
 
-    # find groups
-    groups = {}
-    for n in sys._g.attrs["groups"].keys():
-        g = sys._g.attrs["groups"][n]
-        if g != "":
-            groups[g] = 1
-    # create clusters
-    if group and groups != {}:
-        for g in groups.keys():
-            cconf = copy.deepcopy(bd_conf["cluster"]["default"])
-            if g in bd_conf["cluster"]:
-                for key in bd_conf["cluster"][g]:
-                    cconf[key] = bd_conf["cluster"][g][key]
-            sg = pydot.Subgraph("cluster_" + g, label=g, **cconf)
-            for n in sys._g.attrs["nodes"]:
-                if sys._g.attrs["groups"][n] == g:
-                    add_node(sg, n, bd_conf["node"])
-            graph.add_subgraph(sg)
-    # non-clustered nodes
-    for n in sys._g.attrs["nodes"]:
-        if sys._g.attrs["groups"][n] == "" or not group:
-            add_node(graph, n, bd_conf["node"])
-    # edges
-    p = dict(zip(sys._g.attrs["nodes"].values(), sys._g.attrs["nodes"].keys()))
-    for e in iter(sys._g.edge_indices()):
-        ep = sys._g.get_edge_endpoints_by_index(e)
-        graph.add_edge(pydot.Edge(p[ep[0]], p[ep[1]], **bd_conf["edge"]))
-    # output image
-    if fname == None:
-        img = Image.open(io.BytesIO(graph.create_png(prog="dot")))
-        return img
-    graph.write(fname, prog="dot", format=fname.split(".")[-1])
-    return None
+    The system is first solved to find the losses in each component. If the system has load phases
+    defined, the time-weighted loss is used in the diagram.
+    The default diagram is rendered as a top-bottom diagram with components represented
+    as gradient-colored, square boxes. Shapes and colors can be configured using an attribute dict,
+    although component (node) colors will be overridden by gradient color.
+
+    Parameters
+    ----------
+    fname : str, optional
+        Filename for output image. File extension defines image format.
+    group : bool, optional
+        Cluster components based on group names, by default True.
+    config: dict, optional
+        Graphviz configuration.
+
+    Returns
+    -------
+    None
+        If filename is given.
+    PIL.Image
+        If no filename is given
+
+    Examples
+    --------
+    >>> img = make_hdiag(my_sys)
+    >>> make_hdiag(my_sys, fname="system.svg") # write to file
+    """
+    df = sys.solve()
+    return _diag(sys, fname=fname, group=group, config=config, loss=df)
--- tests/unit/test_diagram.py
+++ tests/unit/test_diagram.py
@@ -24,7 +24,7 @@ import os
 import pytest
 from sysloss.system import System
 from sysloss.components import *
-from sysloss.diagram import get_conf, make_diag, _DEF_CONF
+from sysloss.diagram import get_conf, make_diag, make_hdiag, _DEF_CONF
 
 
 def test_case1():
@@ -34,9 +34,11 @@ def test_case1():
 
 
 def test_case2():
-    """Check make_diag()"""
-    tsys = System("Test system", Source("CR2032", vo=3.0, rs=1))
-    tsys.add_comp("CR2032", comp=VLoss("Diode", vdrop=0.35))
+    """Check make_diag()/make_hdiag()"""
+    tsys = System("Test system", Source("12V", vo=12.0, rs=0.01e-5))
+    tsys.add_comp("12V", comp=VLoss("Diode", vdrop=0.35))
+    tsys.add_comp("12V", comp=PLoad("Bigload", pwr=123, loss=True))
+    tsys.add_comp("12V", comp=PLoad("Bigload2", pwr=454e3, loss=True))
     tsys.add_comp(
         "Diode",
         comp=Converter(
@@ -49,7 +51,8 @@ def test_case2():
             },
         ),
     )
-    tsys.add_comp("Buck 1.8V", comp=PLoad("MCU", pwr=13e-3))
+    tsys.add_comp("Buck 1.8V", comp=PLoad("MCU", pwr=13e-12, loss=True))
+    tsys.add_comp("Buck 1.8V", comp=PLoad("MCU2", pwr=15e-15, loss=True))
     tsys.add_comp(
         "Diode",
         comp=Converter(
@@ -63,10 +66,12 @@ def test_case2():
         "Boost:5V", comp=LinReg("LDO", vo=3.0, ig=0.001, limits={"io": [0, 1.0]})
     )
     tsys.add_comp("LDO", comp=PSwitch("P-switch", rs=0.01))
-    tsys.add_comp("P-switch", comp=ILoad("load 1", ii=0.2), group="loads")
-    tsys.add_comp("P-switch", comp=ILoad("load 2", ii=0.1), group="loads")
-    tsys.add_comp("P-switch", comp=ILoad("load 3", ii=0.07), group="loads")
-    tsys.add_comp("RC filter", comp=RLoad("Sensor", rs=866))
+    tsys.add_comp("P-switch", comp=ILoad("load 1", ii=0.2, loss=True), group="loads")
+    tsys.add_comp("P-switch", comp=ILoad("load 2", ii=0.1e-9, loss=True), group="loads")
+    tsys.add_comp(
+        "P-switch", comp=ILoad("load 3", ii=7.23e-6, loss=True), group="loads"
+    )
+    tsys.add_comp("RC filter", comp=RLoad("Sensor", rs=866, loss=True))
     my_conf = get_conf()
     my_conf["node"]["Source"] = {"shape": "circle", "fillcolor": "coral"}
     my_conf["node"]["Converter"] = {"fillcolor": "darkturquoise"}
@@ -92,3 +97,15 @@ def test_case2():
     assert (
         os.path.exists("tests/unit/Test2.svg") == True
     ), "Check svg file creation, no grouping"
+    # make_hdiag()
+    assert make_hdiag(tsys) != None, "Check heat image generation"
+    ret = make_hdiag(tsys, fname="tests/unit/HTest.svg", config=my_conf)
+    assert ret == None, "Check heat diagram to file"
+    assert (
+        os.path.exists("tests/unit/HTest.svg") == True
+    ), "Check heat svg file creation"
+    tsys_phases = {"stdby": 2155, "move": 12.5, "turn": 0.42}
+    tsys.set_sys_phases(tsys_phases)
+    bl_pwr = {"stdby": 0.1, "move": 455, "turn": 650}
+    tsys.set_comp_phases("Bigload2", phase_conf=bl_pwr)
+    assert make_hdiag(tsys) != None, "Check heat image w/phases generation"
